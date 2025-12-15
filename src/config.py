9cdef598199b645df14b7584a9ea01988bdf8144
Asma Alblooshi <asmaalblooshi@10-20-7-172.dynapool.wireless.nyu.edu>
2025-12-15 16:56:45 -0500

    Add config

---
+++ src/config.py
@@ -0,0 +1,12 @@
+# Project configuration
+
+SEED = 42
+
+# Dataset
+DATASET_NAME = "ag_news"
+TEXT_COLUMN = "text"
+LABEL_COLUMN = "label"
+
+# Limits for faster experiments
+MAX_TRAIN_SAMPLES = 20000
+MAX_TEST_SAMPLES = 5000
